b7c03c2705e1621c8cd3d822b5ce584fe2e4d23b
Teffisk <brad.mcknight91@gmail.com>
2019-02-28 13:10:38 -0800

    added prelim review form

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,4 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../models");
+
+// GET all the reviews for a location
+router.get("/locations/:locationId", (req, res) => {
+  db.review
+    .findAll({ where: { locationId: req.params.locationId } })
+    .then(reviews => {
+      res.send(reviews);
+    })
+    .catch(err => {
+      console.log("Error in the GET REVIEWS route:", err);
+      res.send("Server ERROR!!!");
+    });
+});
+
+//GET all the reviews for a specific user
+
+// POST a new review from a user to a location
+router.post("/new", (req, res) => {
+  db.review
+    .create(req.body)
+    .then(review => {
+      console.log("Review was successfully created!");
+    })
+    .catch(err => {
+      console.log("Error on creating this new review:", err);
+      res.send("Server error!!!");
+    });
+});
 
 module.exports = router;
